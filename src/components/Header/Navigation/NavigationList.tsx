c085e39766c7290f702b51d914b5eb8e230b38df
yuseungjong <fbtmdwhd33@naver.com>
2025-03-07 12:01:54 +0900

    chore: navigation 컴포넌트 분리리

---
+++ src/components/Header/Navigation/NavigationList.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import useUnderline from '@hooks/useUnderline';
+import { setSelectedNavigationIndex } from '@src/store/navigation/navigationSlice';
+import useRedux from '@src/hooks/useRedux';
+
+export default function NavigationList() {
+  const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
+  const [{ list: menuList, selectedIndex }, updater] = useRedux('navigation');
+
+  const underlineStyle = useUnderline(itemRefs, selectedIndex);
+
+  const handleNavigationClick = (index: number) => {
+    updater(setSelectedNavigationIndex, index);
+  };
+
+  return (
+    <>
+      {menuList.map(({ id, to }, index: number) => (
+        <motion.li
+          ref={(el) => {
+            if (el) itemRefs.current[index] = el;
+          }}
+          key={id}
+          className="text-2xl font-bold italic cursor-pointer"
+          whileHover={{
+            scale: 0.8,
+          }}
+        >
+          <Link href={to} onClick={() => handleNavigationClick(index)}>
+            {id}
+          </Link>
+        </motion.li>
+      ))}
+      <motion.div
+        layoutId="underline"
+        className="absolute bottom-[-17px] h-1 rounded-sm bg-[#2D594A] transition-all duration-500"
+        style={{
+          width: `${underlineStyle.width}px`,
+          left: `${underlineStyle.left}px`,
+        }}
+      />
+    </>
+  );
+}
--- src/components/Header/Navigation/index.tsx
+++ src/components/Header/Navigation/index.tsx
@@ -1,49 +1,10 @@
-'use client';
-
-import Link from 'next/link';
-import { motion } from 'framer-motion';
-import { useRef } from 'react';
-import useUnderline from '@hooks/useUnderline';
-import { setSelectedNavigationIndex } from '@src/store/navigation/navigationSlice';
-import useRedux from '@src/hooks/useRedux';
+import NavigationList from './NavigationList';
 
 export default function Navigation() {
-  const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
-  const [{ list: menuList, selectedIndex }, updater] = useRedux('navigation');
-
-  const underlineStyle = useUnderline(itemRefs, selectedIndex);
-
-  const handleNavigationClick = (index: number) => {
-    updater(setSelectedNavigationIndex, index);
-  };
-
   return (
     <nav>
       <ul className="relative flex space-x-6">
-        {menuList.map(({ id, to }, index) => (
-          <motion.li
-            ref={(el) => {
-              if (el) itemRefs.current[index] = el;
-            }}
-            key={id}
-            className="text-2xl font-bold italic cursor-pointer"
-            whileHover={{
-              scale: 0.8,
-            }}
-          >
-            <Link href={to} onClick={() => handleNavigationClick(index)}>
-              {id}
-            </Link>
-          </motion.li>
-        ))}
-        <motion.div
-          layoutId="underline"
-          className="absolute bottom-[-17px] h-1 rounded-sm bg-[#2D594A] transition-all duration-500"
-          style={{
-            width: `${underlineStyle.width}px`,
-            left: `${underlineStyle.left}px`,
-          }}
-        />
+        <NavigationList />
       </ul>
       <span className="material-icons-round text-4xl visible flex lg:hidden">
         menu
