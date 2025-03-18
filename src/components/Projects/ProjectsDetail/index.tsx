2427f0f20fc1ce77cabcbe6ab846b1938ec1c31b
yuseungjong <fbtmdwhd33@naver.com>
2025-03-18 17:06:03 +0900

    feature: project 필터 추가.

--- src/components/Projects/ProjectsDetail/index.tsx
+++ src/components/Projects/ProjectsDetail/index.tsx
@@ -1,6 +1,6 @@
 import useWindowWidth from '@src/hooks/useWindowWidth';
 import { AnimatePresence } from 'framer-motion';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { ProjectsInformationType } from '@src/types/contents/projects';
 import Card from './Card';
 
@@ -138,13 +138,19 @@ export default function ProjectsDetail({
 
   const windowWidth = useWindowWidth();
 
+  useEffect(() => {
+    setSelectedProject(0);
+  }, [selectedProgress]);
+
+  const filterProjects = projectsInformation.filter(
+    ({ progress }) => progress === selectedProgress,
+  );
   const handleOnNextClick = () =>
-    setSelectedProject((prev) => (prev + 1) % projectsInformation.length);
+    setSelectedProject((prev) => (prev + 1) % filterProjects.length);
 
   const handleOnPrevClick = () =>
     setSelectedProject(
-      (prev) =>
-        (prev - 1 + projectsInformation.length) % projectsInformation.length,
+      (prev) => (prev - 1 + filterProjects.length) % filterProjects.length,
     );
 
   const getCardPosition = useCallback(
@@ -155,57 +161,64 @@ export default function ProjectsDetail({
       }
 
       if (index === selectedProject) return 'center';
-      if (index === (selectedProject + 1) % projectsInformation.length)
+      if (index === (selectedProject + 1) % filterProjects.length)
         return 'next';
       if (
         index ===
-        (selectedProject - 1 + projectsInformation.length) %
-          projectsInformation.length
+        (selectedProject - 1 + filterProjects.length) % filterProjects.length
       )
         return 'prev';
       return 'hidden';
     },
-    [selectedProject, windowWidth],
+    [filterProjects.length, selectedProject, windowWidth],
   );
 
   return (
     <div className="py-6 w-full h-full flex flex-col border-2 rounded-lg">
       <div className="lg:relative w-full h-full flex justify-center items-center">
-        <ArrowButton
-          className="left-0"
-          windowWidth={windowWidth}
-          onClick={handleOnPrevClick}
-        >
-          arrow_back_2
-        </ArrowButton>
-        <AnimatePresence>
-          {projectsInformation.map(
-            ({ id, title, skills, progress, image }, index) => {
-              const position = getCardPosition(index);
-              if (position === 'hidden') return null;
+        {filterProjects.length === 0 ? (
+          <div className="font-bold italic">
+            프로젝트 정보가 존재하지 않습니다.
+          </div>
+        ) : (
+          <>
+            <ArrowButton
+              className="left-0"
+              windowWidth={windowWidth}
+              onClick={handleOnPrevClick}
+            >
+              arrow_back_2
+            </ArrowButton>
+            <AnimatePresence>
+              {projectsInformation
+                .filter(({ progress }) => progress === selectedProgress)
+                .map(({ id, title, skills, progress, image }, index) => {
+                  const position = getCardPosition(index);
+                  if (position === 'hidden') return null;
 
-              return (
-                <Card
-                  key={id}
-                  id={id}
-                  position={position}
-                  title={title}
-                  skills={skills}
-                  windowWidth={windowWidth}
-                  progress={progress}
-                  image={image}
-                />
-              );
-            },
-          )}
-        </AnimatePresence>
-        <ArrowButton
-          className="right-0"
-          windowWidth={windowWidth}
-          onClick={handleOnNextClick}
-        >
-          play_arrow
-        </ArrowButton>
+                  return (
+                    <Card
+                      key={id}
+                      id={id}
+                      position={position}
+                      title={title}
+                      skills={skills}
+                      windowWidth={windowWidth}
+                      progress={progress}
+                      image={image}
+                    />
+                  );
+                })}
+            </AnimatePresence>
+            <ArrowButton
+              className="right-0"
+              windowWidth={windowWidth}
+              onClick={handleOnNextClick}
+            >
+              play_arrow
+            </ArrowButton>
+          </>
+        )}
       </div>
     </div>
   );
