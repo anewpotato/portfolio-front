7c527c69bde5e978f1649a93cce04fa3f44ce3a6
yuseungjong <fbtmdwhd33@naver.com>
2025-03-19 17:49:13 +0900

    fix: Project 상세 정보 Modal 반응형 스타일 추가.

--- src/components/Projects/ProjectDetail/ProjectTitle/index.tsx
+++ src/components/Projects/ProjectDetail/ProjectTitle/index.tsx
@@ -7,8 +7,10 @@ export default function ProjectTitle({
   period,
 }: Pick<ProjectsInformationType, 'title' | 'progress' | 'period'>) {
   return (
-    <div className="flex gap-2 items-baseline">
-      <span className="text-3xl font-bold italic">{title}</span>
+    <div className="flex gap-2 items-baseline sm:flex-col lg:flex-row">
+      <span className="sm:text-md md:text-2xl xl:text-3xl font-bold italic">
+        {title}
+      </span>
       <ProgressIcon progress={progress} />
       <span className="text-sm font-semibold text-gray-500">{period}</span>
     </div>
