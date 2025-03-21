686679554de48e480e7c5697a55edead146cc14a
yuseungjong <fbtmdwhd33@naver.com>
2025-03-21 11:12:07 +0900

    fix: Google material icons FOIT 문제 테스트.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export interface CustomIconDescriptorType extends IconDescriptor {
 
 const icon: CustomIconDescriptorType = {
   rel: 'stylesheet',
+  fetchPriority: 'high',
   url: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap',
   precedence: 'default',
 };
